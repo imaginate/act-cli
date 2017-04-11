b93b2f2d4fa626618285a0a1db070d6173a2185f
Adam Smith <adam@imaginate.life>
2017-04-11 10:20:23 -0700

    Added src skeleton

---
+++ src/act-cli.js
@@ -0,0 +1,22 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT CLI
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [act-cli](https://github.com/imaginate/act-cli)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+/// @include ./typedefs.js
+/// @include ./constants.js
+/// @include ./helpers.js
+/// @include ./methods/main.js
+
+module.exports = runActCli;
