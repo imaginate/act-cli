f685865ccbcf621bbe25ca0a64ec7e688d9f7025
Adam Smith <adam@imaginate.life>
2017-04-11 10:23:01 -0700

    Added act exec

---
+++ bin/act.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+/**
+ * -----------------------------------------------------------------------------
+ * ACT CLI
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [act-cli](https://github.com/imaginate/act-cli)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+require('../dist/act-cli.js')(process.argv);
