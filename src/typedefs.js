fc189c3bd14ef719582b425f6a1e7d3c51992c3a
Adam Smith <adam@imaginate.life>
2017-04-11 10:21:22 -0700

    Added current typedefs

---
+++ src/typedefs.js
@@ -0,0 +1,25 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT CLI: TYPEDEFS
+ * -----------------------------------------------------------------------------
+ * @version 1.0.0
+ * @see [act-cli](https://github.com/imaginate/act-cli)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+/**
+ * @typedef {{
+ *   option: string,
+ *   value:  *
+ * }} Option
+ */
+
+/**
+ * @typedef {!Array<!Option>} Options
+ */
+
